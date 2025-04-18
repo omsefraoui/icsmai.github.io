1f0616a4811fc630bf527bc3a06949648bf9b942
omsefraoui <75050504+omsefraoui@users.noreply.github.com>
2025-04-18 22:28:57 +0000

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,21 @@ import Footer from "@/components/layout/footer";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import React from 'react';
+import MenuHamburger from "@/components/MenuHamburger";
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div>
+      <MenuHamburger />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
+
+
 const inter = Inter({ subsets: ["latin"] });
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,20 @@ import Contact from "@/components/contact"
 
 ///////////////////////////////
 
+import '../styles/global.css';
+import type { AppProps } from 'next/app';
+import Layout from "@layout/components/layout";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
+
 
 ///////////////////////////////////
 
