49e252fc9772cf9d4098461748bcc64307fd3f1e
omsefraoui <75050504+omsefraoui@users.noreply.github.com>
2025-04-18 22:31:52 +0000

    test

--- app/page.tsx
+++ app/page.tsx
@@ -11,20 +11,6 @@ import Contact from "@/components/contact"
 
 ///////////////////////////////
 
-import '../styles/global.css';
-import type { AppProps } from 'next/app';
-import Layout from "@layout/components/layout";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
-
-export default MyApp;
-
 
 ///////////////////////////////////
 
