a0fb639993bfbd4bb6a003e2cd36cc837e3907c9
omsefraoui <75050504+omsefraoui@users.noreply.github.com>
2025-04-23 23:04:30 +0000

    test

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Submission from "@/components/submission"
 import Dates from "@/components/dates"
 import Registration from "@/components/registration"
 import Sponsors from "@/components/sponsors"
+import Sponsors from "@/components/proceedings"
 import Contact from "@/components/contact"
 
 export default function Home() {
