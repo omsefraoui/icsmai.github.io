657472a3bdfbd0c2289a96777d901df077efe171
omsefraoui <75050504+omsefraoui@users.noreply.github.com>
2025-09-30 21:19:29 +0000

    test

--- components/contact.tsx
+++ components/contact.tsx
@@ -149,10 +149,11 @@ export default function Contact() {
                   </div>
                   <div className="space-y-1">
                     <p className="font-medium">
-                      The conference will be held in Hotel Radisson Blu Resort Saidia Beach (All Inclusive) in Saïdia, Morocco
+                      ///*The conference will be held in Hotel Radisson Blu Resort Saidia Beach (All Inclusive) in Saïdia, Morocco*/
+                      The conference will be held in Hotel 5 star (All Inclusive) in Saïdia, Morocco
                     </p>
                     <Link
-                      href="https://www.radissonhotels.com/fr-fr/hotels/radisson-blu-resort-saidia-beach"
+                      //href="https://www.radissonhotels.com/fr-fr/hotels/radisson-blu-resort-saidia-beach"
                       className="text-muted-foreground hover:text-primary transition-colors"
                       target="_blank"
                       rel="noopener noreferrer"
